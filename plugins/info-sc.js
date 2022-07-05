59cfa4f66764e989ee7749a90ffc61b5b0760941
レザマスター <107806210+Ikymd@users.noreply.github.com>
2022-07-05 20:17:30 +0700

    Update info-sc.js

--- plugins/info-sc.js
+++ plugins/info-sc.js
@@ -2,7 +2,7 @@ import fetch from 'node-fetch'
 let handler = async(m, { conn, text, usedPrefix, command }) => {
 let pp = await conn.profilePictureUrl(m.chat).catch(_ => null)
 
-let str = `*not available *\nSubscribe Untuk Mengetahui Update Selanjutnya:https://www.instagram.com/sih_bot\nSilahkan follow github saya dibawah ,Terimakasih`
+let str = `https://youtube.com/channel/UCeSakfEaWwtzyrpnkk5_VVA\nSubscribe Untuk Mengetahui Update Selanjutnya:https://www.instagram.com/sih_bot\nSilahkan follow Instagram saya dibawah ,Terimakasih`
 conn.sendButtonDoc(m.chat, str, wm, 'Thanks','Bilek', ftextt, m)
 }
 handler.command = handler.help = ['sc']
