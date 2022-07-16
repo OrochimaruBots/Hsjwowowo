d2e61f4196d6d3c4c90fa3f208922b421c17ca4e
Ikymd <107806210+Ikymd@users.noreply.github.com>
2022-07-16 16:20:24 +0700

    Update group-intro.js

--- plugins/group-intro.js
+++ plugins/group-intro.js
@@ -2,18 +2,34 @@ import fs from 'fs'
 let handler = async (m, {text, usedPrefix, command, conn}) => {
 
 
-let krtu = `0ཻུ۪۪ꦽꦼ̷⸙‹•══════════════♡᭄
-│       *「 Kartu Intro 」*
-│ *Nama     :* 
-│ *Gender   :* 
-│ *Umur      :* 
-│ *Hobby    :* 
-│ *Kelas      :* 
-│ *Asal         :* 
-│ *Agama    :* 
- |  *Status     :* 
-╰═════ꪶ ཻུ۪۪ꦽꦼ̷⸙ ━ ━ ━ ━ ꪶ ཻུ۪۪ꦽꦼ̷⸙
-    *ᴄʜɪᴋᴀ ʙᴏᴛ* 
+let krtu = `Member baru intro
+Nama Panjang:
+Nama Panggilan:
+Umur:
+Kelas:
+Asal:
+Tanggal lahir:
+Temptation lahir:
+Tahun lahir:
+Bulan lahir:
+Nama ayah:
+Nama ibu:
+Nama kakak:
+Nama adek:
+Nama kakek:
+Nama nenek:
+Nama paman:
+Nama bibi:
+Nama sepupu:
+Tinggi badan:
+Berat badan:
+Ukuran sepatu:
+Ukuran baju:
+Ukuran sandal:
+Warna kesukaan:
+Makanan favorit:
+Minuman favorit:
+Emoji favorit:
 `
 conn.fakeReply(m.chat, krtu, '0@s.whatsapp.net', wm)
 }
