a40145584865e68b58407ced9b2e04ad32c82f03
Ikymd <107806210+Ikymd@users.noreply.github.com>
2022-07-17 23:09:28 +0700

    Update group-hidetag.js

--- plugins/group-hidetag.js
+++ plugins/group-hidetag.js
@@ -1,25 +1,15 @@
-let handler = async(m, { conn, text, participants }) => {
-const fkontak = {
-	"key": {
-    "participants":"0@s.whatsapp.net",
-		"remoteJid": "status@broadcast",
-		"fromMe": false,
-		"id": "Halo"
-	},
-	"message": {
-		"contactMessage": {
-			"vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD`
-		}
-	},
-	"participant": "0@s.whatsapp.net"
+import { generateWAMessageFromContent } from '@adiwajshing/baileys'
+
+let handler = async (m, { conn, text, participants }) => {
+  let users = participants.map(u => conn.decodeJid(u.id))
+  let q = m.quoted ? m.quoted : m
+  let c = m.quoted ? m.quoted : m.msg
+  let msg = conn.cMod(m.chat, generateWAMessageFromContent(m.chat, { [c.toJSON ? q.mtype : 'extendedTextMessage']: c.toJSON ? c.toJSON() : { text: c || '' }}, { quoted: null, userJid: conn.user.id }), text || q.text, conn.user.jid, { mentions: users })
+  await conn.relayMessage(m.chat, msg.message, { messageId: msg.key.id })
 }
-     conn.sendMessage(m.chat, { text: text, mentions: participants.map(a => a.id) }, {quoted:fkontak})
-}
-handler.help = ['hidetag <pesan>']
+handler.help = handler.alias = ['hidetag']
 handler.tags = ['group']
 handler.command = /^(hidetag)$/i
+handler.group = handler.admin = true
 
-handler.group = true
-handler.admin = true
-
-module.exports = handler
+export default handler
