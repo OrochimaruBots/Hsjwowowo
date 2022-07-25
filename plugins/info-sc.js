ca1cf8fc7be7cde7446a51c5785d3839cf4fedf6
ᴢɪxʏ.ᴇᴀꜱᴘᴏʀᴛ.xxx <107806210+Ikymd@users.noreply.github.com>
2022-07-25 19:27:44 +0700

    Update info-sc.js

--- plugins/info-sc.js
+++ plugins/info-sc.js
@@ -5,7 +5,7 @@ let pp = await conn.profilePictureUrl(m.chat).catch(_ => null)
 let str = `https://youtube.com/channel/UCeSakfEaWwtzyrpnkk5_VVA\nSubscribe Untuk Mengetahui Update Selanjutnya:https://www.instagram.com/sih_bot\nSilahkan follow Instagram saya dibawah ,Terimakasih`
 conn.sendButtonDoc(m.chat, str, wm, 'Thanks','Bilek', ftextt, m)
 }
-handler.command = handler.help = ['sc']
+handler.command = handler.help = ['sac']
 handler.tags = ['info']
 
 export default handler
